9b3500e0fd46ff47d1a35ae115be42d37ed3ee17
eddyzera <esilva644@gmail.com>
2024-01-09 11:41:34 -0300

    chore: config imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+
 import { Button } from './components/ui/button'
 
 export const App: React.FunctionComponent = () => {
   return (
-   <div className='flex justify-center items-center h-screen'>
-    <Button>Enviar</Button>
-   </div>
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
   )
 }
