78cc53bf2c8996710ce91da9b0d0ba0162429bc5
eddyzera <esilva644@gmail.com>
2024-01-15 14:47:32 -0300

    feat: created page not found

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -43,7 +43,7 @@ export const SignIn: React.FunctionComponent = () => {
         <Button variant={'ghost'} asChild className="absolute right-4 top-4">
           <Link to="/sign-up">Novo estabelecimento</Link>
         </Button>
-        <div className="w-[350px] flex flex-col justify-center gap-6">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Acessar painel
