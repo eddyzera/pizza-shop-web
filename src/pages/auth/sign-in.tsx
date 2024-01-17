d4a489191f67f9f39dd5c96c9fd6814632a6cd87
eddyzera <esilva644@gmail.com>
2024-01-17 12:54:48 -0300

    refactor: using default email if already exists

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query'
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -18,11 +18,16 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export const SignIn: React.FunctionComponent = () => {
+  const [searchParams] = useSearchParams()
   const {
     register,
     handleSubmit,
     formState: { isSubmitted },
-  } = useForm<SignInForm>()
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+    },
+  })
 
   const { mutateAsync: authenticate } = useMutation({
     mutationFn: signIn,
